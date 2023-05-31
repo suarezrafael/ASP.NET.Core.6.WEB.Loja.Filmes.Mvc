9e3552828ced988be49e9d83829cdbdc75e6ddf4
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-05-30 22:30:19 -0300

    Primeiro commit

---
+++ ASP.NET.Core.6.Loja.Filmes.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET.Core._6.Loja.Filmes.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
